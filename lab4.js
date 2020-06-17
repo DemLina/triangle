8aec9da5bc4d5f92c986e38acb042bcf0a59253e
Demianiuk Lina <demianyk.lina@gmail.com>
2020-06-17 13:11:08 +0300

    config

--- lab4.js
+++ lab4.js
@@ -1,11 +1,11 @@
-function Triangle(){
-    let a="";
-    for(let i=0;i<n;i++)
-    {
-        a=(a+"#");
-        document.write(a+'<br>');
+function Triangle() {
+    let a = "";
+
+    for (let i = 0; i < n; i++) {
+        a = (a + "#");
+        document.write(a+ '<br>');
 
     }
 }
-let n = prompt("Введите количество строк",) ;
+let n = prompt("Введите количество строк");
 Triangle();
